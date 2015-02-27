63ee4c017cbb49f07596f51aa6ab15e36fded337
DennisMoosmann <demoosma@htwg-konstanz.de>
2015-02-27 11:15:11 +0100

    Create balls for game

--- public/javascripts/controller/masterCtrl.js
+++ public/javascripts/controller/masterCtrl.js
@@ -3,14 +3,15 @@
  	MasterService.getGrid().then(function(response) {
  		var grid = response.data.grid;
  		var gridLength = grid.length;
+ 		$scope.masterColors = grid[0];
 
- 		var stickArray = new Array(gridLength);
-		for (var i = 0; i < gridLength; i++) {
+ 		var stickArray = new Array(gridLength-1);
+		for (var i = 0; i < gridLength-1; i++) {
 			stickArray[i] = new Array(gridLength/2);
 		}
 
-		var gameArray = new Array(gridLength);
-		for (var i = 0; i < gridLength; i++) {
+		var gameArray = new Array(gridLength-1);
+		for (var i = 0; i < gridLength-1; i++) {
 			gameArray[i] = new Array(gridLength/2);
 		}
 
@@ -20,29 +21,33 @@
 
  		angular.forEach(grid, function(el, i){
         			angular.forEach(el, function(e, j) {
-        				if (j < gridLength/2) {
-        					if (e != null) {
-        						stickArray[rowIndexSticks][j] = e;
-        					} else {
-        						stickArray[rowIndexSticks][j] = '-';
-        					}
+        				if (i != 0) {
+        					if (j < gridLength/2) {
+								if (e != null) {
+									stickArray[rowIndexSticks][j] = e;
+								} else {
+									stickArray[rowIndexSticks][j] = 'gy';
+								}
 
-        				} else {
-        					if (e != null) {
-        						gameArray[rowIndexGame][columnIndexGame] = e;
-        					} else {
-        						gameArray[rowIndexGame][columnIndexGame] = '-';
-        					}
+							} else {
+								if (e != null) {
+									gameArray[rowIndexGame][columnIndexGame] = e;
+								} else {
+									gameArray[rowIndexGame][columnIndexGame] = 'gy';
+								}
 
-        					columnIndexGame++;
+								columnIndexGame++;
+							}
         				}
         			});
-        			columnIndexGame = 0;
-        			rowIndexSticks++;
-        			rowIndexGame++;
+
+        			if (i != 0) {
+        				columnIndexGame = 0;
+                        rowIndexSticks++;
+                        rowIndexGame++;
+        			}
         });
 
-		$scope.masterColors = grid[0];
 		$scope.stickArray = stickArray;
         $scope.gameArray = gameArray;
  	});
