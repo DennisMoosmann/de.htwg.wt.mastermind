5e3e435ecbbe9447bc100430b4c845b0b8db4c79
DennisMoosmann <demoosma@htwg-konstanz.de>
2015-03-05 10:43:03 +0100

    Controller implementation is finished

--- public/javascripts/controller/masterCtrl.js
+++ public/javascripts/controller/masterCtrl.js
@@ -1,4 +1,4 @@
- angular.module('mastermindApp').controller('MasterCtrl', function($scope, $timeout, MasterService) {
+ angular.module('mastermindApp').controller('MasterCtrl', function($scope, MasterService) {
 
     $scope.init = function() {
         $scope.getStatus();
@@ -8,14 +8,8 @@
         $scope.getActualRow();
         $scope.getRowsAmount();
         $scope.getColumnsAmount();
-        //$scope.isSolved();
         $scope.solved = "false";
         $scope.isShown = false;
-
-        $scope.$watch("stickBall", function(){
-            $scope.setButtonPosition(true);
-        });
-
     };
 
     $scope.getStatus = function() {
@@ -27,19 +21,9 @@
 
     $scope.setButtonPosition = function(initial) {
         if (initial) {
-            var stickRows = stickBall.length;
-            var gridCols = $scope.stickArray[0].length;
-            var marginMul = (stickRows/gridCols);
+            var marginMul = ($scope.rowsAmount - 1) * 2;
             var buttonPos = ((((marginMul - 2) * 15) + ((marginMul - 1) * 10)) + 3) + "px";
-
-            if ($scope.rowsAmount == 12) {
-                btnConfirmRow.style.marginTop = buttonPos;
-            } else if ($scope.rowsAmount == 8) {
-                btnConfirmRow.style.marginTop = buttonPos;
-            } else {
-                btnConfirmRow.style.marginTop = buttonPos;
-            }
-
+            btnConfirmRow.style.marginTop = buttonPos;
             $scope.buttonPosition = buttonPos;
 
         } else {
@@ -49,7 +33,7 @@
             btnConfirmRow.style.marginTop = newPos;
             $scope.buttonPosition = newPos;
         }
-    }
+    };
 
 	$scope.getMastermindColors = function() {
         MasterService.getMastermindColors().then(function(response) {
@@ -99,7 +83,7 @@
         MasterService.getRowsAmount().then(function(response) {
             var rowsAmount = response.data;
             $scope.rowsAmount = rowsAmount;
-            //$scope.setButtonPosition(true);
+            $scope.setButtonPosition(true);
         });
     };
 
@@ -114,6 +98,7 @@
 		MasterService.newGame().then(function() {
 		    $scope.init();
 		    btnConfirmRow.disabled = false;
+		    $scope.setButtonPosition(true);
 		});
 	};
 
@@ -172,8 +157,8 @@
 				newCol = $scope.columnsAmount/2 - 1 - col;
 
 				MasterService.setValue(row, newCol, value).then(function(response) {
+				    $scope.getStatus();
 				});
-				$scope.getStatus();
 			}
    		}
      };
@@ -187,24 +172,14 @@
             $scope.solved = conf[1];
             $scope.actualRow = conf[2];
 
-
             if (conf[0] == "true") {
                 if ($scope.solved == "false" && $scope.actualRow != tempRowsAmount) {
-                    //setButtonPosition(false);
+                    $scope.setButtonPosition(false);
                 } else {
                     btnConfirmRow.disabled = true;
                     $scope.showSolution("false");
                 }
-
             }
-
-
-
-            //$scope.getActualRow();
-            //$scope.isSolved();
-
-            //$scope.actualRow = response.data;
-            //$scope.isSolved();
         });
     };
 
@@ -219,6 +194,7 @@
         MasterService.resetSize(rows, cols).then(function(response) {
             $scope.init();
             btnConfirmRow.disabled = false;
+            $scope.setButtonPosition(true);
         });
     };
 
